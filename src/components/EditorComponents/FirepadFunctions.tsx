a2d5c077d58271e8a4e0fb2480f5df967129211b
Pursain <harrisonhuang00@gmail.com>
2020-03-29 01:30:45 -0700

    firepad refactoring

--- src/components/EditorComponents/FirepadFunctions.tsx
+++ src/components/EditorComponents/FirepadFunctions.tsx
@@ -1,10 +1,7 @@
-
-// generates a roomId to give to Editor as prop
-// use this function when creating the login page for the interviewer
-export function makeRoomId(apiKey: String, databaseUrl: String): String {
+// Initialize Firebase if not already
+export function initializeFirebase(apiKey: String, databaseUrl: String){
   // @ts-ignore
   if (!window.firebase.apps.length) {
-    // check if firebase has already been initialized
     var config = {
       apiKey: apiKey,
       databaseURL: databaseUrl
@@ -12,9 +9,22 @@ export function makeRoomId(apiKey: String, databaseUrl: String): String {
     // @ts-ignore
     window.firebase.initializeApp(config);
   }
+}
 
+// generates a roomId to give to Editor as prop
+// use this function when creating the login page for the interviewer
+export function makeRoomId(apiKey: String, databaseUrl: String): String {
+  initializeFirebase(apiKey, databaseUrl);
   // @ts-ignore
   var ref = window.firebase.database().ref();
   ref = ref.push()
   return ref.key
+}
+
+// Get Firepad room reference from Firebase
+export function getFirebaseRoomRef(roomId: String){
+    // @ts-ignore
+    var ref = window.firebase.database().ref();
+    ref = ref.child(roomId);
+    return ref;
 }
