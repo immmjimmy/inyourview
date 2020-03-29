45149d9998fb178c19fd351c459df78e00a2b3db
Jeffrey Khong <khong.jeffrey@gmail.com>
2020-03-29 00:58:39 -0700

    Host and Join Frontend
    implemented the host and join pages

--- src/components/LandingComponents/Host.tsx
+++ src/components/LandingComponents/Host.tsx
@@ -3,13 +3,84 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Host = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Button>Hi</Button>
+    <Container fluid style = {{height: "100%", maxHeight: "100%"}}>
+      <Row style = {{height: "5%", backgroundColor: "#7189ff"}}>
+        <Col style = {{height: "100%"}}>
+          <h1
+            style={{
+              textAlign: "left",
+              marginTop: "15px",
+              fontFamily: "Expletus Sans",
+              color: "white",
+              fontSize: "17px",
+            }}>
+            <img
+              src="/ft logo.png"
+              style={{ width: "17px", height: "auto" }}
+            />
+            In Your View
+          </h1>
         </Col>
       </Row>
-      {/* add more rows/columns as necessary */}
+
+      <Row style = {{height: "92.5%", backgroundColor: "white"}}>
+        <Col style = {{height: "100%"}}>
+          <Row style = {{height:"7.5%"}}>
+            <Col style = {{height: "100%"}}>
+                <h1
+                  style={{
+                    textAlign: "center",
+                    marginTop: "30px",
+                    marginBottom: "30px",
+                    marginLeft: "30px",
+                    fontFamily: "Raleway",
+                    color: "black",
+                    fontSize: "30px",
+                    fontWeight: "bold",
+                  }}>
+                  Host Room
+                </h1>
+            </Col>
+          </Row>
+          <Row style = {{height:"50%"}}>
+            <Col>
+            </Col>
+            <Col>
+              <Form style = {{marginTop: "30px", marginLeft: "45px", fontFamily:" Raleway"}}>
+                <Form.Label>Interviewer Name</Form.Label>
+                <Form.Group controlId="formInterviewerName">
+                <Form.Control
+                  type="text" placeholder="Enter your name" style={{borderRadius: "10px"}}
+                  />
+                </Form.Group>
+                <Form.Label>Room Name</Form.Label>
+                <Form.Group controlId="formRoomName">
+                  <Form.Control
+                    type="text" placeholder = "Enter room name" style={{borderRadius: "10px"}}
+                    />
+                </Form.Group>
+                <Form.Label>Room Passcode</Form.Label>
+                <Form.Group controlId="formRoomPasscode">
+                  <Form.Control
+                    type="text" placeholder = "Enter room passcode" style={{borderRadius: "10px"}}
+                    />
+                </Form.Group>
+                <Form.Label>Interviewee Name</Form.Label>
+                <Form.Group controlId="formIntervieweeName">
+                <Form.Control
+                  type="text" placeholder="Enter interviewee's name" style={{borderRadius: "10px"}}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" style={{marginTop: "25px", borderRadius: "10px", width:"100px", textAlign: "center"}}>
+                  Host
+                </Button>
+              </Form>
+            </Col>
+            <Col>
+            </Col>
+          </Row>
+      </Col>
+    </Row>
     </Container>
   );
 };
