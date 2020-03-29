1d3cc4bb2a3e72e0bd43d125284aaf5245ec7fef
Gina <Jasonleh99@gmail.com>
2020-03-28 18:15:04 -0700

    Connection established

--- server.js
+++ server.js
@@ -6,11 +6,25 @@ const AccessToken = require("twilio").jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 require("dotenv").config();
 
+// MongoDB stuff
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const BodyParser = require("body-parser");
+
+const mongoConnectUser = process.env.MONGO_CONNECT_USER;
+const mongoConnectPass = process.env.MONGO_CONNECT_PASS;
+const CONNECTION_URL = "mongodb+srv://" + mongoConnectUser + ':' + mongoConnectPass + "@lahacks2020-0apqi.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+const DATABASE_NAME = "inyourview";
+
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
 const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/token", (req, res) => {
@@ -38,4 +52,16 @@ app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"))}
 );
 
-app.listen(8081, () => console.log("token server running on 8081"));
+app.listen(8081, () => {
+    console.log("token server running on 8081");
+
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      throw error;
+    }
+
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("interviews");
+    console.log("Connected to '" + DATABASE_NAME + "'!");
+  });
+});
